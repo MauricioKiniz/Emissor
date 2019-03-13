f40b8e0f52aabb3490bd67f74a499ea7adb4332e
Mauricio <mksistemaseconsultoria@gmail.com>
2019-03-12 23:07:05 -0300

    Atualizr projetos

---
+++ Projetos/MKSistemas.Biblioteca/Funcoes.fs
@@ -0,0 +1,21 @@
+﻿namespace MKSistemas
+
+module Validacao =
+    open System
+
+    // funcoes genericas de validacao
+    let stringVaziaOuNula str = 
+        match str with
+        | Some str -> System.String.IsNullOrEmpty(str)
+        | None -> true
+
+    let stringComDoisOuMaisCaracteres str = 
+        if stringVaziaOuNula str then 
+            false 
+        else 
+            str.Value.Length > 2
+    
+    let stringNula str = 
+        match str with
+        | Some str -> (str = null)
+        | None -> true
---
+++ Projetos/MKSistemas.Biblioteca/Tipos.fs
@@ -0,0 +1,14 @@
+﻿namespace MKSistemas
+    open System
+
+    type Identificador = Guid   
+    type RecursoId = Identificador
+    type NomeBase = string
+    type DescricaoBase = string
+
+    type Requisicao<'data> =
+        {
+            Data: 'data;
+            DataRequisicao: DateTime;
+            IdUsuario: Identificador
+        }
